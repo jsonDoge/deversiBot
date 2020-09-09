7d71ac16fa08f791f3a6faf233e9f1f2ef7cf609
jsonDoge <16711523+jsonDoge@users.noreply.github.com>
2020-09-09 14:05:19 +0300

    placement range initial implementation with testing

--- config.example.js
+++ config.example.js
@@ -1,5 +1,6 @@
 const config = {
   orderbookUrl: 'https://api.deversifi.com/bfx/v2/book/tETHUSD/R0',
+  orderRange: 5, // percent
 };
 
 module.exports = config;
--- test/index.js
+++ test/index.js
@@ -1,31 +1,44 @@
 const { expect } = require('chai');
 
-const { _getSpreadMargins } = require('../index');
+const { _getSpreadMargins, _getPlacementRange } = require('../index');
 
-describe('getSpreadMargins', function () {
-  it('should throw if no asks', function () {
-    const orders = [[0, 3, 1], [0, 5, 1], [0, 4, 1]];
+describe('bot', function () {
+  describe('getSpreadMargins', function () {
+    it('should throw if no asks', function () {
+      const orders = [[0, 3, 1], [0, 5, 1], [0, 4, 1]];
 
-    expect(() => _getSpreadMargins(orders)).to.throw('No asks');
+      expect(() => _getSpreadMargins(orders)).to.throw('No asks');
+    });
+
+    it('should throw if no bids', function () {
+      const orders = [[0, 3, -1], [0, 5, -1], [0, 4, -1]];
+
+      expect(() => _getSpreadMargins(orders)).to.throw('No bids');
+    });
+
+    it('should return highest bid value in mixed value array', function () {
+      const orders = [[0, 1, 1], [0, 3, 1], [0, 2, 1], [0, 6, -1]];
+
+      const { highestBid } = _getSpreadMargins(orders);
+      expect(highestBid).to.equal(3);
+    });
+
+    it('should return lowest ask value in mixed value array', function () {
+      const orders = [[0, 3, -1], [0, 5, -1], [0, 4, -1], [0, 2, 1]];
+
+      const { lowestAsk } = _getSpreadMargins(orders);
+      expect(lowestAsk).to.equal(3);
+    });
   });
 
-  it('should throw if no bids', function () {
-    const orders = [[0, 3, -1], [0, 5, -1], [0, 4, -1]];
+  describe('getPlacementRange', function () {
+    it('should be within the provided percentage in bigNumbers', function () {
+      const orderRange = 8;
+      const bestValue = 10;
 
-    expect(() => _getSpreadMargins(orders)).to.throw('No bids');
-  });
-
-  it('should return highest bid value in mixed value array', function () {
-    const orders = [[0, 1, 1], [0, 3, 1], [0, 2, 1], [0, 6, -1]];
-
-    const { highestBid } = _getSpreadMargins(orders);
-    expect(highestBid).to.equal(3);
-  });
-
-  it('should return lowest ask value in mixed value array', function () {
-    const orders = [[0, 3, -1], [0, 5, -1], [0, 4, -1], [0, 2, 1]];
-
-    const { lowestAsk } = _getSpreadMargins(orders);
-    expect(lowestAsk).to.equal(3);
+      const { high, low } = _getPlacementRange(bestValue, orderRange);
+      expect(high.toFixed()).to.equal('10.4');
+      expect(low.toFixed()).to.equal('9.61538461538461538462');
+    });
   });
 });
