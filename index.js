ef6d66fc08b21e1eaa6578a9d61eaffd788e80d2
jsonDoge <16711523+jsonDoge@users.noreply.github.com>
2020-09-09 13:29:21 +0300

    move margin functionality to separate function and add tests

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 const fetch = require('node-fetch');
 const { orderbookUrl } = require('./config');
 
+function getSpreadMargins(orders) {
+  let asks = [];
+  let bids = [];
+  orders.forEach((o) => {
+    if (o[2] < 0) { asks.push(o); return; }
+    bids.push(o);
+  });
+
+  if (bids.length === 0) {
+    throw new Error('No bids');
+  }
+
+  if (asks.length === 0) {
+    throw new Error('No asks');
+  }
+
+  const highestBid = bids.reduce((h, bid) => h = bid[1] > h ? bid[1] : h, bids[0][1]);
+  const lowestAsk = asks.reduce((l, ask) => l = ask[1] < l ? ask[1] : l, asks[0][1]);
+
+  return { highestBid, lowestAsk };
+}
+
 async function main () {
   const res = await fetch(orderbookUrl);
   if (res.status !== 200) {
@@ -8,21 +30,14 @@ async function main () {
     return;
   }
   const orders = await res.json();
-  let asks = [];
-  let bids = [];
-  orders.forEach((o) => {
-    if (o[2] < 0) { asks.push(o); return; }
-    bids.push(o);
-  });
-  
-  console.info('bids: ', bids);
-  console.info('asks: ', asks);
-
-  const highestBid = bids.reduce((h, bid) => h = bid[1] > h ? bid[1] : h, bids[0][1]);
-  const lowestAsk = asks.reduce((l, ask) => l = ask[1] < l ? ask[1] : l, asks[0][1]);
+  const { highestBid, lowestAsk } = getSpreadMargins(orders);
 
   console.info('highest bid: ', highestBid);
   console.info('lowest ask: ', lowestAsk);
 }
 
 main();
+
+module.exports = {
+  _getSpreadMargins: getSpreadMargins,
+};
